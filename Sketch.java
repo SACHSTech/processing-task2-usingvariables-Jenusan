69ca630b0a3065d21fde1673a3c010bf80be3894
= <=>
2022-02-23 14:02:13 -0500

    final

--- Sketch.java
+++ Sketch.java
@@ -10,8 +10,8 @@ public class Sketch extends PApplet {
 	// put your size call here
 
 
-  int width = 800;
-  int height = 800;
+   int width = 1000;
+  int height = 1000;
     size(width, height);
   }
 
@@ -27,66 +27,62 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff\
 
-    //sun
     int sunLength = (int)(height/2);
     int sunWidth = (int)(width/2);
-    fill(255,255,0);
-    ellipse(width,0,sunWidth,sunLength);
-
-    //foreground
-    //casting values to ints
     int ground = (int)(height*0.75);
-    fill(0,255,0);
-    rect(0,ground, width,height);
-
-    //purple flower
     int purpFlowerMidX = (int)(width/2);
     int FlowerStem1y = (int)(height*0.75);
-    int FlowerStem2y = (int)(height*0.55);
-    line(purpFlowerMidX, FlowerStem1y, purpFlowerMidX, FlowerStem2y);
-
+    int FlowerMid2y = (int)(height*0.55);
     int petalLength = (int)(height/7);
     int pistalSize = (int)(width/20);
+    int redFlowerMidX = (int)(width*0.75);
+    int blueFlowerMidX = (int)(width/4);
+
+    //sun
+    fill(255, 255, 0);
+    ellipse(width, 0, sunWidth, sunLength);
+
+    //foreground
+    //casting values to int
+    fill(0, 255, 0);
+    rect(0,ground, width, height);
+
+    //purple flower
+    line(purpFlowerMidX, FlowerStem1y, purpFlowerMidX, FlowerMid2y);
 
     fill(102,0,153);
-    ellipse(purpFlowerMidX,FlowerStem2y,petalLength,pistalSize);
+    ellipse(purpFlowerMidX,FlowerMid2y,petalLength,pistalSize);
 
     fill(102,0,153);
-    ellipse(purpFlowerMidX,FlowerStem2y,pistalSize,petalLength);
+    ellipse(purpFlowerMidX,FlowerMid2y,pistalSize,petalLength);
 
     fill(255,255,0);
-    ellipse(purpFlowerMidX,FlowerStem2y,pistalSize,pistalSize);
+    ellipse(purpFlowerMidX,FlowerMid2y,pistalSize,pistalSize);
 
     //red flower
-    int redFlowerMidX = (int)(width*0.75);
-    line(redFlowerMidX, FlowerStem1y, redFlowerMidX, FlowerStem2y);
-
-
+    line(redFlowerMidX, FlowerStem1y, redFlowerMidX, FlowerMid2y);
 
     fill(255,0,0);
-    ellipse(redFlowerMidX,FlowerStem2y,petalLength,pistalSize);
+    ellipse(redFlowerMidX,FlowerMid2y,petalLength,pistalSize);
 
     fill(255,0,0);
-    ellipse(redFlowerMidX,FlowerStem2y,pistalSize,petalLength);
+    ellipse(redFlowerMidX,FlowerMid2y,pistalSize,petalLength);
 
     fill(255,255,0);
-    ellipse(redFlowerMidX,FlowerStem2y,pistalSize,pistalSize);
+    ellipse(redFlowerMidX,FlowerMid2y,pistalSize,pistalSize);
 
     //blue flower
-    int blueFlowerStemx = (int)(width/4);
-    line(blueFlowerStemx, FlowerStem1y, blueFlowerStemx, FlowerStem2y);
+    line(blueFlowerMidX, FlowerStem1y, blueFlowerMidX, FlowerMid2y);
 
     fill(0,0,255);
-    ellipse(blueFlowerStemx,FlowerStem2y,petalLength,pistalSize);
+    ellipse(blueFlowerMidX,FlowerMid2y,petalLength,pistalSize);
 
     fill(0,0,255);
-    ellipse(blueFlowerStemx,FlowerStem2y,pistalSize,petalLength);
+    ellipse(blueFlowerMidX,FlowerMid2y,pistalSize,petalLength);
 
     fill(255,255,0);
-    ellipse(blueFlowerStemx,FlowerStem2y,pistalSize,pistalSize);
+    ellipse(blueFlowerMidX,FlowerMid2y,pistalSize,pistalSize);
 
   }
   
